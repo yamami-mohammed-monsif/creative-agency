2ca5b90a6c4536882622886b5c8ed691328233a0
yamami mohamed monsif <mohammedyamami05@gmail.com>
2024-12-23 08:11:10 +0000

    updated the homepage and base scripts

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Creative-Agency-landing-page/",
+  base: "/creative-agency/",
   plugins: [react()],
 });
